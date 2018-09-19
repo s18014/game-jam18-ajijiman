ff1d0f19e1e256a139f2de7f70cd207a95753e14
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-09-19 15:26:01 +0900

    テスト用のプレイヤーとドラゴンのスクリプト、攻撃とタイマーのスクリプトを作成

---
+++ Assets/Script/Dragon_At.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon_At : MonoBehaviour {
+
+    private GameObject target;
+
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Player");
+	 	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(target.transform.position);
+		
+	}
+}
