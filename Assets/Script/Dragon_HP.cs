ee954bcb2477c451ceb360f65d0744e93a146aac
HiroyaMasuko <c18015@std.it-college.ac.jp>
2018-09-20 14:00:49 +0900

    ドラゴンのHPとそれのスクリプトの追加

--- Assets/Script/Dragon_HP.cs
+++ Assets/Script/Dragon_HP.cs
@@ -1,23 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Dragon_HP : MonoBehaviour
 {
+    public nemuHP nemuHp;
 
-    public int nemuHP = 100;
+    public int nemuHP ;
     public int okoHP = 0;
+
+    public int DangoDM;
+    public int PizzaDM;
+    public int NikumanDM;
+
+    public int NEAT;
+
+    Slider _slider;
 
 
     // Use this for initialization
     void Start()
     {
+        _slider = GameObject.Find("nemu_Gauge").GetComponent<Slider>();
 
-        if (nemuHP < 0)
+        _slider.value = nemuHP;
+
+        if (nemuHP < 100)
             nemuHP = 0;
 
-        if (okoHP < 50)
-            okoHP = 50;
+        if (okoHP < 70)
+            okoHP = 0;
 
     }
 
@@ -25,12 +39,57 @@ public class Dragon_HP : MonoBehaviour
     void Update()
     {
 
+        nemuHp.Addnemuhp(NEAT);
+
+
 
 
     }
-    public void AddTimer(int amount)
-    {
-        nemuHP -= amount;
+
+
+    void OnTriggerStay2D(Collider2D other)
+    { 
+   
+        if (other.gameObject.tag == "Dango")
+        {
+
+            nemuHP += DangoDM;
+                      
+            Destroy(other.gameObject);
+        }
+
+
+        if (other.gameObject.tag == "Pizza")
+        {
+            nemuHP += PizzaDM;
+            
+            Destroy(other.gameObject);
+        }
+
+
+        if (other.gameObject.tag == "Nikuman")
+        {
+            nemuHP += NikumanDM;
+            
+            Destroy(other.gameObject);
+        }
 
     }
+
+
+    /*
+        public void dango ()
+        {
+            nemuHp.AddDangoo(DangoDM);
+        }
+
+        public void pizza ()
+        {
+            nemuHp.AddPizza(PizzaDM);
+        }
+
+        public void nikuman ()
+        {
+            nemuHp.AddNikuman(NikumanDM);
+        }*/
 }
---
+++ Assets/Script/nemuHP.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class nemuHP : MonoBehaviour {
+
+
+    Slider _slider;
+
+    // Use this for initialization
+    void Start () {
+        _slider = GameObject.Find("nemu_Gauge").GetComponent<Slider>();
+
+    }
+    float nemuHp = 0;
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+
+
+
+
+
+        _slider.value = nemuHp;
+    }
+
+    public void Addnemuhp(int amount)
+    {
+        nemuHp += amount;
+    }
+
+    /*
+    public void AddDangoo(int amount)
+    {
+        nemuHp += amount;
+    }
+
+    public void AddPizza(int amount)
+    {
+        nemuHp += amount;
+    }
+
+    public void AddNikuman(int amount)
+    {
+        nemuHp += amount;
+    }*/
+}
