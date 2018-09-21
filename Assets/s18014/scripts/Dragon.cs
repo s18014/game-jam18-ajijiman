b1c5ecb33672c14b1bfbcbacef5db905e657c2f0
s18014 <s18014@std.it-college.ac.jp>
2018-09-21 16:12:47 +0900

    aaa

---
+++ Assets/s18014/scripts/Dragon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour {
+    public GameObject[] bulletPrefabs;
+    float time = 3f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("shot");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator shot ()
+    {
+        yield return new WaitForSeconds(2f);
+        while(true)
+        {
+            Instantiate(bulletPrefabs[Random.Range(0, 2)]);
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
