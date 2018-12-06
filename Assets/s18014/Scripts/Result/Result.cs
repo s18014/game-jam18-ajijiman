9c1dad7800b745ac29e5e76cea47a520c8a731d5
s18014 <s18014@std.it-college.ac.jp>
2018-12-07 00:27:00 +0900

    リザルト画面を少し勧めた

--- Assets/s18014/Scripts/Result/Result.cs
+++ Assets/s18014/Scripts/Result/Result.cs
@@ -10,14 +10,34 @@ public class Result : MonoBehaviour {
     Text BonusText;
     Text BarText;
     Text ScoreText;
+    int score = 0;
+    float time;
+    int life;
+    int angryCount;
+    bool isFinalAttackClear;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        BonusText = transform.Find("BonusText").gameObject.GetComponent<Text>();
+        RemainingLifeText = transform.Find("RemainingLifeText").gameObject.GetComponent<Text>();
+        ScoreText = transform.Find("ScoreText").gameObject.GetComponent<Text>();
+        AngryTimesText = transform.Find("AngryTimesText").gameObject.GetComponent<Text>();
+        life = GameController.getLife();
+        angryCount = GameController.getAngryCount();
+        isFinalAttackClear = GameController.getIsFinalAttackClear();
+        if (isFinalAttackClear)
+        {
+            BonusText.text = "限界を超えた: +10000";
+            score += 10000;
+        }
+        AngryTimesText.text += " " + angryCount + "回     +" + angryCount * 1000;
+        score += angryCount * 1000;
+        RemainingLifeText.text += " " + life + "個     +" + life * 3000;
+        score += life * 3000;
+        ScoreText.text += " " + score;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }
--- Assets/s18014/scripts/GameController.cs
+++ Assets/s18014/scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
     Text gameStatusText;
@@ -11,6 +12,13 @@ public class GameController : MonoBehaviour {
     GameObject APManager;
     bool isGameClear = false;
     bool isGameOver = false;
+    bool sceneSwitch = false;
+
+    // Propaty for ResultScene
+    public static float time;
+    public static int lifePoint;
+    public static int angryCount;
+    public static bool isFinalAttackClear = false;
 
 	// Use this for initialization
 	void Start () {
@@ -25,7 +33,31 @@ public class GameController : MonoBehaviour {
 	void Update () {
         checkGameStatus();
         showGameStatus();
-	}
+        if ((isGameOver || isGameClear) && !sceneSwitch)
+        {
+            lifePoint = player.hp;
+            angryCount = dragon.angryCount;
+            if (angryCount >= 4)
+            {
+                isFinalAttackClear = true;
+            }
+            StartCoroutine("moveResultScene");
+            sceneSwitch = true;
+        }
+    }
+
+    public static int getLife () {
+        return lifePoint;
+    }
+
+    public static int getAngryCount () {
+        return angryCount;
+    }
+
+    public static bool getIsFinalAttackClear () {
+        return isFinalAttackClear;
+    }
+
 
     void checkGameStatus () {
         if (dragon.hungryPoint <= 0f) {
@@ -38,15 +70,18 @@ public class GameController : MonoBehaviour {
     void showGameStatus () {
         if (isGameClear) {
             gameStatusText.text = "GAME CLEAR";
-            stopControll();
-        } else if (isGameOver) {
+            bulletManager.enabled = false;
+        }
+        else if (isGameOver) {
             gameStatusText.text = "GAME OVER";
-            stopControll();
+            bulletManager.enabled = false;
+            APManager.SetActive(false);
         }
     }
 
-    void stopControll () {
-        bulletManager.enabled = false;
-        APManager.SetActive(false);
+    IEnumerator moveResultScene ()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Result");
     }
 }
